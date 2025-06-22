f73aa663d873cd930697d119b29f0fddaa076cac
YuriLeonel <tbeater66@hotmail.com>
2025-06-22 20:33:59 -0300

    feat: create DS first structure and clean up initial project garbage

---
+++ components/Button/Button.tsx
@@ -0,0 +1,14 @@
+type ButtonProps = {
+    children: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+
+
+const Button = ({children, className, ...props}: ButtonProps) => {
+  return <button 
+        className={`${className} bg-accent1-primary text-white px-4 py-2 rounded-md`}
+        {...props}
+        >{ children }</button>
+}
+
+export default Button
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'bg-light': 'var(--bg-light)',
+        'bg-dark': 'var(--bg-dark)',
+        'bg-disabled': 'var(--bg-disabled)',
+        'text-primary': 'var(--text-primary)',
+        'text-secondary': 'var(--text-secondary)',
+        'text-tertiary': 'var(--text-tertiary)',
+        'text-disabled': 'var(--text-disabled)',
+        'icons': 'var(--icons)',
+        'borders': 'var(--borders)',
+        'dividers': 'var(--dividers)',
+        'boxes': 'var(--boxes)',
+        'accent1-primary': 'var(--accent1-primary)',
+        'accent1-secondary': 'var(--accent1-secondary)',
+        'accent1-tertiary': 'var(--accent1-tertiary)',
+        'accent1-quaternary': 'var(--accent1-quaternary)',
+        'accent1-hover': 'var(--accent1-hover)',
+        'accent1-click': 'var(--accent1-click)',
+        'accent2-primary': 'var(--accent2-primary)',
+        'accent2-secondary': 'var(--accent2-secondary)',
+        'accent2-tertiary': 'var(--accent2-tertiary)',
+        'accent2-quaternary': 'var(--accent2-quaternary)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
